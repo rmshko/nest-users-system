e3dc444c1c0c2412f1260e1cecf8a8e4726f70af
Roman Romashcko <roman.romashcko@yandex.ru>
2021-08-06 09:40:06 +0300

    Add jwt secret to config

--- src/config/index.ts
+++ src/config/index.ts
@@ -4,6 +4,7 @@ const {
   DB_PORT = '27017',
   DB_USER = 'admin',
   DB_PASSWORD = 'superuser',
+  SECRET_KEY = 'default-secret',
 } = process.env;
 
 const mongoDbUri = `mongodb://${DB_USER}:${DB_PASSWORD}@${DB_HOST}:${DB_PORT}`;
@@ -14,5 +15,8 @@ export default () => ({
     users: {
       uri: `${mongoDbUri}/users`,
     },
+  },
+  jwt: {
+    secret: SECRET_KEY,
   },
 });
